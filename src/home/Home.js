e6cc731c99e93d9f669048f377d3668a6d974b7f
Kelby Blakeslee <kelby.blakeslee@yahoo.com>
2018-06-15 15:11:36 -0600

    finished styling shelf buttons

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './home.css';
 
 
 export default function Home() {
     return (
-        <div>
-            <div>
-                <Link to="/ShelfA" >Shelf A</Link>
+        <div className="Div-around-buttons">
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"></link>
+            <div className="Shelf-A">
+                <Link to="/ShelfA" ><button className="Shelf-Button">Shelf A</button></Link>
             </div>
-            <div>
-                <Link to="/ShelfB" >Shelf B</Link>
+            <div className="Shelf-B">
+                <Link to="/ShelfB" ><button className="Shelf-Button">Shelf B</button></Link>
             </div>
-            <div>
-                <Link to="/ShelfC" >Shelf C</Link>
+            <div className="Shelf-C">
+                <Link to="/ShelfC" ><button className="Shelf-Button">Shelf C</button></Link>
             </div>
-            <div>
-                <Link to="/ShelfD" >Shelf D</Link>
+            <div className="Shelf-D">
+                <Link to="/ShelfD" ><button className="Shelf-Button">Shelf D</button></Link>
             </div>
         </div>
     )
