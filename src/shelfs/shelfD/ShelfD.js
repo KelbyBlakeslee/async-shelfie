c20f3eedd0acf7f5e22ae0efe830e5b12398172a
Kelby Blakeslee <kelby.blakeslee@yahoo.com>
2018-06-15 12:09:51 -0600

    finished adding set bins

--- src/shelfs/shelfD/ShelfD.js
+++ src/shelfs/shelfD/ShelfD.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 
@@ -8,15 +9,27 @@ class ShelfD extends Component {
         super();
 
         this.state = {
-
+            bin_id: {}
         }
+    }
+
+    getBinId(bin_id) {
+        axios.get('/api/view_bins/:id', {'fullBin': this.state.bin_id, 'binId': bin_id})
+        .then(response => {
+            console.log(response)
+        })
     }
 
 
     render() {
         return (
             <div>
-                <Link to="/Bins"><button>Bin 1</button></Link>
+                <Link to="/Bins"><button onClick={() => this.getBinId('D1')}>Bin 1</button></Link>
+                <Link to="/Bins"><button onClick={() => this.getBinId('D2')}>Bin 2</button></Link>
+                <Link to="/Bins"><button onClick={() => this.getBinId('D3')}>Bin 3</button></Link>
+                <Link to="/Bins"><button onClick={() => this.getBinId('D4')}>Bin 4</button></Link>
+
+
             </div>
         )
     }
