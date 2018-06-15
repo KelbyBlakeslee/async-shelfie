ed5a6418a2be8a0c4ab159e01fb3d4adac111ca5
Kelby Blakeslee <kelby.blakeslee@yahoo.com>
2018-06-15 12:29:20 -0600

    full bins on shelfs

--- src/shelfs/shelfA/ShelfA.js
+++ src/shelfs/shelfA/ShelfA.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
+import './shelfA.css';
 
 
 class ShelfA extends Component {
@@ -14,21 +14,20 @@ class ShelfA extends Component {
     }
 
     getBinId(bin_id) {
-        axios.get('/api/view_bins/:id', {'fullBin': this.state.bin_id, 'binId': bin_id})
-        .then(response => {
-            console.log(response)
-        })
+        axios.get('/api/view_bins/:id', { 'fullBin': this.state.bin_id, 'binId': bin_id })
+            .then(response => {
+                console.log(response)
+            })
     }
 
 
     render() {
         return (
             <div>
-                <Link to="/Bins"><button onClick={() => this.getBinId('A1')}>Bin 1</button></Link>
-                <Link to="/Bins"><button onClick={() => this.getBinId('A2')}>Bin 2</button></Link>
-                <Link to="/Bins"><button onClick={() => this.getBinId('A3')}>Bin 3</button></Link>
-                <Link to="/Bins"><button onClick={() => this.getBinId('A4')}>Bin 4</button></Link>
-
+                <Link to="/Bins"><button className="Bin-button" onClick={() => this.getBinId('A1')}>Bin 1</button></Link>
+                <Link to="/Bins"><button className="Bin-button" onClick={() => this.getBinId('A2')}>Bin 2</button></Link>
+                <Link to="/Bins"><button className="Bin-button" onClick={() => this.getBinId('A3')}>Bin 3</button></Link>
+                <Link to="/Bins"><button className="Bin-button" onClick={() => this.getBinId('A4')}>Bin 4</button></Link>
             </div>
         )
     }
